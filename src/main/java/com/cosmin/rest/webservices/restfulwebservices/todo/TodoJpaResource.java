fa0348283132c3534a8ca92368038ab7d8d693ea
Cosmin Dinu <cdcdd15@gmail.com>
2021-04-17 12:01:41 +0300

    Modified hello world endpoint.

--- src/main/java/com/cosmin/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
+++ src/main/java/com/cosmin/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
@@ -23,7 +23,7 @@ public class TodoJpaResource {
 
 	@GetMapping("/hello-world")
 	public ResponseEntity<String> hello() {
-		return new ResponseEntity<String>("Hello world updated", HttpStatus.OK);
+		return new ResponseEntity<String>("Hello world updated 2", HttpStatus.OK);
 //		return ResponseEntity.ok().build();
 	}
 
